57a48595b649a5d2608038d7d39d23f6979e50fe
Mukul Rai <137355469+rai-mukul@users.noreply.github.com>
2024-02-10 22:28:50 -0600

    bug fix - unique code

--- model/productModel.js
+++ model/productModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     code: {
         type: Number, 
-        required: true
+        required: true,
+        unique: true
     },
     name: {
         type: String,
